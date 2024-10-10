5f7e762f7f94825174bc54447e6832462b4a0886
Mick L <micklito.dev@gmail.com>
2024-10-10 02:27:38 -0400

    change connection string

--- config/connection.ts
+++ config/connection.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 mongoose.set("strictQuery", false);
-mongoose.connect("mongodb://mongo-db:27017/graphql-test").then((e) => {
+mongoose.connect("mongodb://@mongo:27017/graphql-test").then((e) => {
   console.log("error/connected", e);
 });
 
