3c0d90e7fc89c09f57acfcf82cd01f4a5c2c8fd4
Mick L <micklito.dev@gmail.com>
2024-10-10 02:41:52 -0400

    create process.env uri string for aws secrets.

--- config/connection.ts
+++ config/connection.ts
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
 
 mongoose.set("strictQuery", false);
-mongoose.connect("mongodb://@mongo:27017/graphql-test").then((e) => {
-  console.log("error/connected", e);
-});
+mongoose
+  .connect(process.env.MONGODB_URI || "mongodb://mongo-db:27017/graphql-test")
+  .then(() => {
+    console.log("data connected to db ");
+  });
 
 export default mongoose.connection;
