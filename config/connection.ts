b807d191d0daab8143d5d2d6fe0bdf10e6a0a1a6
Mick L <micklito.dev@gmail.com>
2024-10-10 01:44:23 -0400

    test changing default string connextion string modify entry point of pkgjson

--- config/connection.ts
+++ config/connection.ts
@@ -1,11 +1,8 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
-dotenv.config();
 
 mongoose.set("strictQuery", false);
-
 mongoose
-  .connect(process.env.MONGODB_URI || "mongodb://mongo-db:27017/graphql-test")
+  .connect(process.env.MONGODB_URI || "mongodb://mongo:27017/graphql-test")
   .then(() => {
     console.log("data connected to db ");
   });
