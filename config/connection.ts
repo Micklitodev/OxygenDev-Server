6e2a4b61100a9f63b2f8a6c758e47bf9c4812101
Mick L <micklito.dev@gmail.com>
2024-10-10 03:06:33 -0400

    changesecret name

--- config/connection.ts
+++ config/connection.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 mongoose.set("strictQuery", false);
 mongoose
-  .connect(process.env.MONGODB_URI || "mongodb://mongo-db:27017/graphql-test")
+  .connect(process.env.MONGO_DB_URI || "mongodb://mongo-db:27017/graphql-test")
   .then(() => {
     console.log("data connected to db ");
   });
